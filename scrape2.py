6ac695970d5f77114b599204236c75e49a9dda59
caxios <90167107+caxios@users.noreply.github.com>
2023-11-16 17:32:07 +0900

    Update scrape2.py

--- scrape2.py
+++ scrape2.py
@@ -3,6 +3,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
+import os
+import requests
 
 link_lst = []
 """
@@ -19,7 +21,7 @@ def get_image_links(keyword, repeat):
     options.add_argument("--start-maximized")
     driver = webdriver.Chrome(options=options)
     driver.get(f"https://www.google.com/search?q={keyword}&tbm=isch")
-    time.sleep(2)
+    #time.sleep(2)
     for i in range(repeat):
         driver.find_elements(By.CLASS_NAME, "rg_i")[i].click()
         time.sleep(2)
@@ -30,13 +32,22 @@ def get_image_links(keyword, repeat):
         link_lst.append(a_tag[i].find("img", class_="sFlh5c")['src'])
         time.sleep(1)
     driver.quit()
-    return link_lst
+    #return link_lst
+
+def create_files(keyword, links):
+    for i, link in enumerate(links):
+        response = requests.get(link)
+        filename = f"image of {keyword}_{i}.jpg"
+        with open(filename, "wb") as f:
+            f.write(response.content)
+        print(f"File {filename} created successfully!")
 
 def main():
     keyword = "<any keyword>"
     repeat = 2
-    lst = get_image_links(keyword, repeat)
-    print(lst)
+    get_image_links(keyword, repeat)
+    #print(lst)
+    create_files(keyword, link_lst)
 
 if __name__ == "__main__":
     main()
